7f742400013601a10794ab96f2bef7890290a8c8
Hadi493 <hadialam493@gmail.com>
2025-12-24 10:06:37 +0600

    ...

--- main.c
+++ main.c
@@ -41,8 +41,10 @@ void generate_rays(struct Circle circle, struct Ray rays[RAYS_NUMBER]) {
   }
 }
 
-void FillRays(SDL_Surface *surface, struct Ray rays[RAYS_NUMBER],
-              Uint32 color) {
+void FillRays(SDL_Surface *surface, struct Ray rays[RAYS_NUMBER], Uint32 color,
+              struct Circle object) {
+
+  double radius_squared = pow(object.r, 2);
   for (int i = 0; i < RAYS_NUMBER; i++) {
     struct Ray ray = rays[i];
 
@@ -62,6 +64,12 @@ void FillRays(SDL_Surface *surface, struct Ray rays[RAYS_NUMBER],
         end_of_screen = 1;
       if (y_draw < 0 || y_draw > HEIGHT)
         end_of_screen = 1;
+
+      double distance_squared =
+          pow(x_draw - object.x, 2) + pow(y_draw - object.y, 2);
+      if (distance_squared < radius_squared) {
+        break;
+      }
     }
   }
 }
@@ -97,7 +105,7 @@ int main() {
     SDL_FillRect(surface, &erase_rect, COLOR_BLACK);
     FillCircle(surface, circle, COLOR_WHITE);
     FillCircle(surface, shadow_circle, COLOR_WHITE);
-    FillRays(surface, rays, COLOR_GRAY);
+    FillRays(surface, rays, COLOR_GRAY, shadow_circle);
     SDL_UpdateWindowSurface(window);
     SDL_Delay(10);
   }
